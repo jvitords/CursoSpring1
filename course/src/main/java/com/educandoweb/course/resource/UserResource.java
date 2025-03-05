c13bcfb19a907f4af026cc5c7f364ec49a78ea79
João Vitor Santos <jvduarte.santos@hotmail.com>
2025-03-05 12:31:22 -0300

    Implementando a função rest do user

--- course/src/main/java/com/educandoweb/course/resource/UserResource.java
+++ course/src/main/java/com/educandoweb/course/resource/UserResource.java
@@ -1,5 +1,19 @@
 package com.educandoweb.course.resource;
 
-public class UserResource {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.educandoweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> encontrarUsuarios() {
+		User user = new User(1L, "João Vitor", "joao@gmail.com", "993045596", "J.vitor10");
+		return ResponseEntity.ok().body(user);
+	}
 }
